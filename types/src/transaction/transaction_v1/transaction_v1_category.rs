ddc6fef6cea99ab66633e956f2bf4710fbc40bc6
Ed Hastings <ed@casperlabs.io>
2024-02-27 21:46:05 -0800

    WIP rework 184

--- types/src/transaction/transaction_v1/transaction_v1_category.rs
+++ types/src/transaction/transaction_v1/transaction_v1_category.rs
@@ -1,39 +1,53 @@
 use core::fmt::{self, Formatter};
 
+use crate::Deploy;
 #[cfg(feature = "datasize")]
 use datasize::DataSize;
 #[cfg(feature = "json-schema")]
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-/// The category of a [`Transaction`].
-#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Serialize, Deserialize, Debug)]
+/// The category of a Transaction.
+#[derive(
+    Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Serialize, Deserialize, Debug, Default,
+)]
 #[cfg_attr(feature = "datasize", derive(DataSize))]
 #[cfg_attr(
     feature = "json-schema",
     derive(JsonSchema),
-    schemars(description = "Session kind of a Transaction.")
+    schemars(description = "Session kind of a V1 Transaction.")
 )]
 #[serde(deny_unknown_fields)]
 #[repr(u8)]
-pub enum TransactionV1Category {
+pub enum TransactionCategory {
+    /// Standard transaction (the default).
+    #[default]
+    Standard = 0,
+    /// Native mint interaction.
+    Mint = 1,
+    /// Native auction interaction.
+    Auction = 2,
     /// Install or Upgrade.
-    InstallUpgrade,
-    /// Standard transaction.
-    Standard,
-    /// Staking-related transaction.
-    Staking,
-    /// Transfer.
-    Transfer,
+    InstallUpgrade = 3,
 }
 
-impl fmt::Display for TransactionV1Category {
+impl fmt::Display for TransactionCategory {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            TransactionV1Category::InstallUpgrade => write!(f, "InstallUpgrade"),
-            TransactionV1Category::Standard => write!(f, "Standard"),
-            TransactionV1Category::Staking => write!(f, "Staking"),
-            TransactionV1Category::Transfer => write!(f, "Transfer"),
+            TransactionCategory::Standard => write!(f, "Standard"),
+            TransactionCategory::Mint => write!(f, "Mint"),
+            TransactionCategory::Auction => write!(f, "Auction"),
+            TransactionCategory::InstallUpgrade => write!(f, "InstallUpgrade"),
+        }
+    }
+}
+
+impl From<&Deploy> for TransactionCategory {
+    fn from(value: &Deploy) -> Self {
+        if value.is_transfer() {
+            TransactionCategory::Mint
+        } else {
+            TransactionCategory::Standard
         }
     }
 }
