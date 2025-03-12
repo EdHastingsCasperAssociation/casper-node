b43408341983e5afe3c2fe05f70fc14f1f020e2f
Ed Hastings <ed@casper.network>
2025-03-12 08:33:50 -0700

    remove unused scheduling

--- types/src/transaction/transaction_scheduling.rs
+++ types/src/transaction/transaction_scheduling.rs
@@ -36,10 +36,6 @@ use serde::{Deserialize, Serialize};
 pub enum TransactionScheduling {
     /// No special scheduling applied.
     Standard,
-    /// Execution should be scheduled for the specified era.
-    FutureEra(EraId),
-    /// Execution should be scheduled for the specified timestamp or later.
-    FutureTimestamp(Timestamp),
 }
 
 impl TransactionScheduling {
@@ -47,18 +43,6 @@ impl TransactionScheduling {
         match self {
             TransactionScheduling::Standard => {
                 vec![crate::bytesrepr::U8_SERIALIZED_LENGTH]
-            }
-            TransactionScheduling::FutureEra(era_id) => {
-                vec![
-                    crate::bytesrepr::U8_SERIALIZED_LENGTH,
-                    era_id.serialized_length(),
-                ]
-            }
-            TransactionScheduling::FutureTimestamp(timestamp) => {
-                vec![
-                    crate::bytesrepr::U8_SERIALIZED_LENGTH,
-                    timestamp.serialized_length(),
-                ]
             }
         }
     }
@@ -68,8 +52,6 @@ impl TransactionScheduling {
     pub fn random(rng: &mut TestRng) -> Self {
         match rng.gen_range(0..3) {
             0 => TransactionScheduling::Standard,
-            1 => TransactionScheduling::FutureEra(EraId::random(rng)),
-            2 => TransactionScheduling::FutureTimestamp(Timestamp::random(rng)),
             _ => unreachable!(),
         }
     }
@@ -79,30 +61,12 @@ const TAG_FIELD_INDEX: u16 = 0;
 
 const STANDARD_VARIANT: u8 = 0;
 
-const FUTURE_ERA_VARIANT: u8 = 1;
-const FUTURE_ERA_ERA_ID_INDEX: u16 = 1;
-
-const FUTURE_TIMESTAMP_VARIANT: u8 = 2;
-const FUTURE_TIMESTAMP_TIMESTAMP_INDEX: u16 = 1;
-
 impl ToBytes for TransactionScheduling {
     fn to_bytes(&self) -> Result<Vec<u8>, Error> {
         match self {
             TransactionScheduling::Standard => {
                 CalltableSerializationEnvelopeBuilder::new(self.serialized_field_lengths())?
                     .add_field(TAG_FIELD_INDEX, &STANDARD_VARIANT)?
-                    .binary_payload_bytes()
-            }
-            TransactionScheduling::FutureEra(era_id) => {
-                CalltableSerializationEnvelopeBuilder::new(self.serialized_field_lengths())?
-                    .add_field(TAG_FIELD_INDEX, &FUTURE_ERA_VARIANT)?
-                    .add_field(FUTURE_ERA_ERA_ID_INDEX, &era_id)?
-                    .binary_payload_bytes()
-            }
-            TransactionScheduling::FutureTimestamp(timestamp) => {
-                CalltableSerializationEnvelopeBuilder::new(self.serialized_field_lengths())?
-                    .add_field(TAG_FIELD_INDEX, &FUTURE_TIMESTAMP_VARIANT)?
-                    .add_field(FUTURE_TIMESTAMP_TIMESTAMP_INDEX, &timestamp)?
                     .binary_payload_bytes()
             }
         }
@@ -125,24 +89,6 @@ impl FromBytes for TransactionScheduling {
                 }
                 Ok(TransactionScheduling::Standard)
             }
-            FUTURE_ERA_VARIANT => {
-                let window = window.ok_or(Formatting)?;
-                window.verify_index(FUTURE_ERA_ERA_ID_INDEX)?;
-                let (era_id, window) = window.deserialize_and_maybe_next::<EraId>()?;
-                if window.is_some() {
-                    return Err(Formatting);
-                }
-                Ok(TransactionScheduling::FutureEra(era_id))
-            }
-            FUTURE_TIMESTAMP_VARIANT => {
-                let window = window.ok_or(Formatting)?;
-                window.verify_index(FUTURE_TIMESTAMP_TIMESTAMP_INDEX)?;
-                let (timestamp, window) = window.deserialize_and_maybe_next::<Timestamp>()?;
-                if window.is_some() {
-                    return Err(Formatting);
-                }
-                Ok(TransactionScheduling::FutureTimestamp(timestamp))
-            }
             _ => Err(Formatting),
         };
         to_ret.map(|endpoint| (endpoint, remainder))
@@ -153,10 +99,6 @@ impl Display for TransactionScheduling {
     fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
         match self {
             TransactionScheduling::Standard => write!(formatter, "schedule(standard)"),
-            TransactionScheduling::FutureEra(era_id) => write!(formatter, "schedule({})", era_id),
-            TransactionScheduling::FutureTimestamp(timestamp) => {
-                write!(formatter, "schedule({})", timestamp)
-            }
         }
     }
 }
